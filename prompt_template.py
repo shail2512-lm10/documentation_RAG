ace2053aa4d39477274ef8028d41fe681fb7738f
Shail Shah <shail251298@gmail.com>
2024-08-15 19:42:22 +0000

    modified prompt template

--- prompt_template.py
+++ prompt_template.py
@@ -1,9 +1,9 @@
 qa_prompt_tmpl_str = (
-"Context information is below.\n"
-"---------------------\n"
-"{context}\n"
-"---------------------\n"
-"Given the context information above I want you to think step by step to answer the query in a crisp manner, incase case you don't know the answer say 'I don't know!'.\n"
+"I want you to think step by step to answer the below query in a crisp manner.\n"
 "Query: {query}\n"
+"And if you require any help from context to answer the query, below is some context for you.\n"
+"{context}"
+"----------"
+"Incase case you don't know the answer only say 'I don't know!'.\n"
 "Answer: "
 )
